a68c5fc5dcb31783a83a4f950880952980c237e6
Manuel Cuevas <admin@vikingosol.com>
2017-07-20 10:17:26 -0600

    Add Query Log

--- src/DataBot.php
+++ src/DataBot.php
@@ -55,6 +55,7 @@ class DataBot
 		}
 		file_put_contents("php://stderr", "GetLocation\n" );
 		$sql = "SELECT dir,hours FROM Places WHERE name  like '". $Place . "'";
+		file_put_contents("php://stderr", "QUERY ". $sql . "\n" );
 		if ($res1 = $myConn2->query($sql)) {
 			if ($res1->num_rows === 0) {
 			    // ¡Oh, no ha filas! Unas veces es lo previsto, pero otras
