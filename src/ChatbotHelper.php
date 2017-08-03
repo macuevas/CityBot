7f7542b7f8b9984d8af5050514650001c1acb0ee
Manuel Cuevas <admin@vikingosol.com>
2017-08-02 22:32:33 -0600

    fix err

--- src/ChatbotHelper.php
+++ src/ChatbotHelper.php
@@ -313,7 +313,7 @@ Stories, songs, and play for families. Our evening program offers terrific books
                 $tmp["date"]=$ev["start_time"];
                 $eventos[]=$tmp;
             }
-            file_put_contents("php://stderr", print_r($eventos,true));
+            
             $noev=1;
             foreach ($eventos as &$ev2) {
                 if ($noev>=10)
@@ -325,6 +325,7 @@ Stories, songs, and play for families. Our evening program offers terrific books
                             ]);
                 $noev=$noev + 1;
             }
+            file_put_contents("php://stderr", print_r($respuesta,true));
             #$chatbotHelper->send($senderId,"Great!!!");
             $this->send($senderId,"I Find de next events:");
             $this->sendMsj(new StructuredMessage($senderId,
