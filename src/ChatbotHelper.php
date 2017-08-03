63e12ee73d284f764339cc7d7d56671b75494f9b
Manuel Cuevas <admin@vikingosol.com>
2017-08-02 23:12:19 -0600

    FIX

--- src/ChatbotHelper.php
+++ src/ChatbotHelper.php
@@ -324,7 +324,7 @@ Stories, songs, and play for families. Our evening program offers terrific books
                 {
                     break;
                 }
-                $response2 = $fb->get('/'.$ev2["id"].'/picture?redirect=false'); 
+                $response2 = $fb->get('/'.$ev2["id"].'/picture?redirect=false&type=large'); 
                 $resimg=$response2->getDecodedBody();
                 $respuesta []= new MessageElement($ev2["name"],"[".$ev2["date"]."]".$ev2["description"], $resimg["data"]["url"], [
                                             new MessageButton(MessageButton::TYPE_POSTBACK, 'View')                                         
