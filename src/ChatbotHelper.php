85fe7812c41e023032988f8174bda30f2a7d5d56
Manuel Cuevas <admin@vikingosol.com>
2017-08-07 15:02:42 -0600

    Fix Err

--- src/ChatbotHelper.php
+++ src/ChatbotHelper.php
@@ -481,7 +481,7 @@ class ChatbotHelper
                 $botones[] = new MessageButton(MessageButton::TYPE_WEB, 'View',"https://www.facebook.com/".$pag["fb_id"],"compact") ;
                 if ($pag["tel"]!="")
                 {
-                     $botones[] = new MessageButton("phone_number" , 'Call',$pag["tel"],"compact") ;
+                    // $botones[] = new MessageButton("phone_number" , 'Call',$pag["tel"],"compact") ;
                 }
                 $respuesta []= new MessageElement($pag["name"],"", $pag["url"], [
                                             $botones                                       
