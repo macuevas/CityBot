d982ed3acacfdd823f76dd1de451c087917e2f55
Manuel Cuevas <admin@vikingosol.com>
2017-08-21 16:17:11 -0600

    Fix Err

--- bot/index.php
+++ bot/index.php
@@ -158,7 +158,7 @@ if ($senderId && $chatbotHelper->isMessage())
             break;
             case 'CMD_READY':
                 #$chatbotHelper->send($senderId,"What are you looking for today?"); 
-                $chatbotHelper->send( new StructuredMessage($senderId,
+                $chatbotHelper->sendMsj( new StructuredMessage($senderId,
                       StructuredMessage::TYPE_BUTTON,
                       [
                           'text' => 'What are you looking for today?',
