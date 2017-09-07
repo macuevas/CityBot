d0fd1ccd029093449fe76695484c854ff83ae745
Manuel Cuevas <admin@vikingosol.com>
2017-09-06 21:56:56 -0600

    fix Err

--- src/ChatbotHelper.php
+++ src/ChatbotHelper.php
@@ -362,11 +362,11 @@ class ChatbotHelper
                 #foreach ($eventos as &$ev2) {
                 file_put_contents("php://stderr", "Count == ".count($eventos)." $n==".$noevent);
                 $evshow=4;
-                if (count($eventos)-($noevent+4)==1)
+                if ((count($eventos)-($noevent+4))==1)
                 {
                     $evshow=3;
                 }
-                for ($n=$noevent; $n < $noevent+$$evshow=4;; $n++)
+                for ($n=$noevent; $n < $noevent+$$evshow=4; $n++)
                 {
                     if (count($eventos)<= $n)
                     {
